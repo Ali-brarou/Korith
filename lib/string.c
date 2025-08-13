dcd63c42f9c858e75979a63c2bd1a0ac022d45a3
Ali-brarou <110550922+Ali-brarou@users.noreply.github.com>
2025-08-14 00:42:56 +0100

    Update string.c

--- lib/string.c
+++ lib/string.c
@@ -1,8 +1,7 @@
 #include <stddef.h> 
 #include <korith/string.h> 
 
-/* generic fallback */
-#ifndef ARCH_HAS_STRLEN 
+
 size_t __strlen(const char *str)
 {
     const char *p = str;
@@ -11,26 +10,4 @@ size_t __strlen(const char *str)
 
     return p - str;
 }
-#endif
 
-#ifndef ARCH_HAS_MEMSET 
-void *__memset(void *s, int c, size_t n)
-{
-    uint8_t *p = s; 
-    while (n--)
-        *p++ = (uint8_t)c; 
-
-    return s; 
-}
-#endif 
-
-#ifndef ARCH_HAS_MEMCPY 
-void *__memcpy(void *dest, void *src, size_t n)
-{
-    uint8_t *p = dest; 
-    while(n--)
-        *p++ = *(uint8_t*)src++; 
-
-    return dest; 
-}
-#endif
