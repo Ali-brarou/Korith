6f2f582e0b25cbd609a82a2533acc23138d96b06
Ali-Brarou <alibrarou16@gmail.com>
2025-10-07 09:46:45 +0100

    added TODO

--- include/korith/segments.h
+++ include/korith/segments.h
@@ -1,9 +1,9 @@
 #ifndef _KORITH_SEGMENTS_H_
 #define _KORITH_SEGMENTS_H_
 
-#define KERNEL_CS   0x08
-#define KERNEL_DS   0x10
-#define USER_CS     0x18
-#define USER_DS     0x20
+#define __KERNEL_CS   0x08
+#define __KERNEL_DS   0x10
+#define __USER_CS     0x18
+#define __USER_DS     0x20
 
 #endif
